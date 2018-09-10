97f4d0e32659014b3fa45e20435090361118b495
panguangfeng <jason.pan@onedollartech.com>
2018-09-10 17:02:51 +0800

    新提交

--- sechand-web/app/main.js
+++ sechand-web/app/main.js
@@ -2,13 +2,18 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Header from './component/header/header';
-import Input from './component/input/input_area'
+import Input from './component/input/input_area';
+import Content from  './component/content/content';
+import Underside from './component/underside/underside'
+
 import './main.css';//使用require导入css文件
 
 render(
     <div>
         <Header/>
         <Input/>
+        <Content/>
+        <Underside/>
     </div>,
         document.getElementById('root')
 )
